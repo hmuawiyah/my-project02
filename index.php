12e8b693f3ef13df8743107dc2ead919cc700967
huseinm <macbookpro@huseinm.local>
2022-11-30 15:11:56 +0700

    fitur tambahan

--- index.php
+++ index.php
@@ -69,6 +69,7 @@ if(isset($_POST['textAreaLink'])){
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/08bcd19637.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -160,7 +161,7 @@ if(isset($_POST['textAreaLink'])){
         <label for="floatingTextarea">Caption</label>
         <div class="d-flex justify-content-between mt-2">
             <div>                
-                <button class=" btn btn-sm btn-outline-primary copyBtn " >Copy Caption</button>
+                <button class=" btn btn-sm btn-outline-primary copyBtn " >Copy Caption <i class="fa-sharp fa-solid fa-copy"></i></button>
             </div>
             <div>
                 <button class=" btn btn-sm btn-outline-primary simpleCaptionBtn" >Simple Caption</button>
@@ -190,8 +191,8 @@ if(isset($_POST['textAreaLink'])){
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <a href="https://www.bolasport.com/bola" class="btn btn-outline-primary btn-sm" target="_blank">BolaSport</a>
-        <a href="https://bola.kompas.com/" class="btn btn-outline-primary btn-sm" target="_blank">Bola Kompas</a>
+        <a href="https://www.bolasport.com/bola" class="btn btn-outline-primary btn-sm mb-2" target="_blank">BolaSport <i class="fa-sharp fa-solid fa-arrow-up-right-from-square"></i></a><br/>
+        <a href="https://bola.kompas.com/" class="btn btn-outline-primary btn-sm mb-2" target="_blank">Bola Kompas <i class="fa-sharp fa-solid fa-arrow-up-right-from-square"></i></a><br/>
         <p class="mt-3" >Pilih portal berita, salin link lalu paste pada kolom link dan klik kirim</p>
       </div>
       <div class="modal-footer">
